581f7a0c331265d337031a26117cad3ebdf8d7be
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-12-09 11:12:50 -0800

    Offer model

---
+++ server/models/Offer.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var offerSchema = mongoose.Schema({
+  from: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  to: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  production: {type: mongoose.Schema.Types.ObjectId, ref: 'Production'},
+  status: String,
+  position: String,
+  rate: Number,
+  hours: Number,
+  dateFrom: Date,
+  dateTo: Date,
+  // TODO: message: [{type: mongoose.Schema.Types.ObjectId, ref: 'Message'}]
+}, { timestamps: true });
