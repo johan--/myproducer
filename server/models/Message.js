e40bac64433d4185dddabcb6e8b9cd963ac7bfb5
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-12-09 15:59:43 -0800

    Fix syntax error on Message model

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-var Message = mongoose.Schaema({
-  _by: {type: mongoose.Schema.Type.ObjectId, ref: 'User'}
+var Message = mongoose.Schema({
+  _by: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   content: String,
   active: {type: Boolean, default: true},
 }, { timestamps: true })
