b3b1102a84bf298e38858c7c77e3f108e47df186
Alex Karevoll <alexkarevoll@Alexs-MacBook-Pro-2.local>
2016-12-12 16:26:04 -0800

    patch crew from production view via hire btn

--- client/controllers/productionCtrl.js
+++ client/controllers/productionCtrl.js
@@ -18,7 +18,7 @@ function productionController($http, $stateParams, $state, ProductionFactory, Au
       $http.patch('/api/productions/' + $stateParams.id, vm.production)
         .success(function(data) {
           vm.editing = false
-          $state.reload();
+          $state.reload()
         })
     }
 
@@ -26,6 +26,24 @@ function productionController($http, $stateParams, $state, ProductionFactory, Au
       $http.delete('/api/productions/' + $stateParams.id)
         .success(function(data) {
           $state.go('production-list')
+        })
+    }
+
+    vm.makeOffer = function(id) {
+      vm.offer = {
+        offer : {
+          status : 'Pending',
+          position : vm.production.offer.position,
+          rate : vm.production.offer.rate,
+          hours : vm.production.offer.hours
+        }
+      }
+      // TODO this patches the offer, but somehow that doesn't reflect within the production object? Do we need to save the production as well on this patch?
+      $http.patch('api/crew/' + id, vm.offer)
+        .success(function(data) {
+          // $state.go('offer')
+          $state.reload()
+          console.log("data after patch", data)
         })
     }
 }
