7f673bab5a7056a2471524390c1451c38b98cf09
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-12-19 18:46:19 -0500

    Show pending contacts

--- client/controllers/otherProfileCtrl.js
+++ client/controllers/otherProfileCtrl.js
@@ -8,8 +8,25 @@ otherProfileController.$inject = ['$http', '$stateParams', '$state', 'Production
 function otherProfileController($http, $stateParams, $state, ProductionFactory, AuthService){
   var vm = this;
 
-  $http.get('/api/users/' + $stateParams.id)
+  vm.currentUser = {}
+  AuthService.getUserStatus()
     .then(function(data){
-      vm.user = data.data
-    })
+      vm.currentUser = data.data.user
+      // console.log(data.data.user)
+      $http.get('/api/users/' + vm.currentUser._id)
+        .success(function(data){
+          vm.currentUser = data
+          console.log(data);
+
+
+          $http.get('/api/users/' + $stateParams.id)
+            .then(function(data){
+              vm.user = data.data
+
+              vm.isPending = vm.currentUser.pendingContacts.find(function(pc) {
+                return pc._id === vm.user._id
+              })
+            })
+        })
+  })
 }
