b966894783f57f5fd15f6eea9b5b74efa5cc6700
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-12-24 02:39:57 -0500

    Disable editing on past productions

--- client/controllers/productionCtrl.js
+++ client/controllers/productionCtrl.js
@@ -158,4 +158,13 @@ function productionController($rootScope, $http, $stateParams, $state, Productio
     vm.closeNotifModal = function() {
       vm.notifModal.show = false
     }
+
+    vm.compareDate = function(date){
+      date = new Date(date)
+      date.setDate(date.getDate() + 1)
+      if(!date){
+        return false
+      }
+      return new Date() < date
+    }
 }
