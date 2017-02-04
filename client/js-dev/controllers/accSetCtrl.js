1de52084cb61a04db34d17aae0f19144cabcddb0
Garrett Estrin <garrett.estrin@gmail.com>
2017-02-04 15:01:13 -0800

    stash

---
+++ client/js-dev/controllers/accSetCtrl.js
@@ -0,0 +1,16 @@
+// angular.module('myApp')
+//   .controller('accountSettingController', accountSettingController)
+//
+// accountSettingController.$inject = ['$rootScope', '$state', '$stateParams', 'AuthService']
+//
+// // Complete Registration CONTROLLER
+// function accountSettingController($rootScope, $state, $stateParams, AuthService){
+//   console.log("accountSettingController instantiated");
+//   var vm = this
+//   vm.disabled = false
+// 
+//   vm.changePassword = function(vm.password){
+//     vm.disabled = true
+//
+//   }
+// }
