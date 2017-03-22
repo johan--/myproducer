86e54be1a07f699fa7fc90d38bfaede797ce35d8
Matthew Alon <alonmatthew94@gmail.com>
2017-03-22 12:39:32 -0700

    fixed extra spacing

--- client/js-dev/controllers/postCtrl.js
+++ client/js-dev/controllers/postCtrl.js
@@ -29,7 +29,6 @@ function postController($rootScope, $http, $stateParams, $state, AuthService, $s
       $http.get('/api/users/' + data.data.user._id + '/profile')
         .success(function(data){
           vm.currentUser = data
-          vm.userWebsite = vm.currentUser.website
           initialRows(vm.currentUser.bio)
           // get all productions where I am a crew member
           var otherProductions = []
