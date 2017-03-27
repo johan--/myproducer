ac7e45f8cc50d7ce98dd1c355c4273e7937a81ec
Matthew Alon <alonmatthew94@gmail.com>
2017-03-27 11:26:52 -0700

    verified offer ctrl gave gulp minify error

--- client/js-dev/controllers/verifiedOfferCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('MyApp')
-  .controller('verifiedOfferController', verifiedOfferController)
-
-verifiedOfferConroller.$inject = ['$rootScope', 'AuthService', '$http', '$stateParams', '$state', '$mixpanel']
-
-function verifiedOfferController($rootScope, AuthService, $http, $stateParams, $state, $mixpanel)
