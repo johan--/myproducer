92ec5da099a88e53fa9ed59ad76c2a22de0f248d
Matthew Alon <alonmatthew94@gmail.com>
2017-04-20 13:20:35 -0700

    styling changes to question mark boxes

--- client/js-dev/controllers/postCtrl.js
+++ client/js-dev/controllers/postCtrl.js
@@ -7,9 +7,9 @@ postController.$inject = ['$rootScope', '$http', '$stateParams', '$state', 'Auth
 
 function postController($rootScope, $http, $stateParams, $state, AuthService, $scope, $mixpanel){
   var vm = this
-  vm.hoverPending = false
-  vm.hoverCurrent = false
-  vm.hoverPast = false
+  vm.hoverPending = true
+  vm.hoverCurrent = true
+  vm.hoverPast = true
   vm.currentUser = {}
   vm.editingState = false;
   vm.fileInput = document.getElementById('file-input')
